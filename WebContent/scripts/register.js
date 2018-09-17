afc1819c5dfed5a5fd8054a83800d02fd8aae26f
tmjmkk@gmail.com <tmjmkk@gmail.com>
2018-09-17 16:29:41 +0900

    권경태 16시 29분

---
+++ WebContent/scripts/register.js
@@ -0,0 +1,16 @@
+$(function() {
+	$('#form-register').submit(function(e) {
+		e.preventDefault();
+		$(this).find('.txt-warning').empty().hide();
+		var email = $('#inp-email').val();
+		if (!validateEmail(email)) {
+			$('#inp-email').next().html('잘못된 형식입니다.').show();
+			return;
+		}
+
+	});
+});
+function validateEmail(email) {
+var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+return re.test(email);
+}
