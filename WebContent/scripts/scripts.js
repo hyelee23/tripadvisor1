fb2f3202b9727f2681c663e1fef975cc6cc3f170
ty9409 <ty9409@gmail.com>
2018-09-18 11:12:31 +0900

    받지마세요

--- WebContent/scripts/scripts.js
+++ WebContent/scripts/scripts.js
@@ -8,4 +8,18 @@ $(function(){
          $('#header').removeClass('inverted');
       }
    });
+   $(window).trigger('scroll');
+   var dpFrom = $('#from').datepicker({
+	   dateFormat:'yy-mm-dd',
+	   minDate: 0
+   });
+   dpFrom.datepicker('setDate', new Date());
+   var dpTo = $('#to').datepicker({
+	   dateFormat: 'yy-mm-dd',
+	   minDate: 0,
+	   onSelect: function() {
+		   dpTo.datepicker('option','minDate',dpFrom.datepicker('getDate'));
+	   }
+   });
+   dpTo.datepicker('setDate', 4);
 });
